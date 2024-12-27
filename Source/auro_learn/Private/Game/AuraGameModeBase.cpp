13580616fd4409c357226ab6be67ef0e0e71039f
yesteryard <13965906+yeasteryard@user.noreply.gitee.com>
2024-12-27 20:48:27 +0800

    player controller, input mapping and gamemode 0

---
+++ Source/auro_learn/Private/Game/AuraGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright LeZhu Gaming
+
+
+#include "Game/AuraGameModeBase.h"
+
---
+++ Source/auro_learn/Public/Game/AuraGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright LeZhu Gaming
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AuraGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURO_LEARN_API AAuraGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
